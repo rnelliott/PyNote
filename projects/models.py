77d506192b7623c446b61c03ca043f0a5d7a05e9
Dave O'Dea <hi@daveodea.com>
2018-10-15 20:44:24 +0100

    Fix bug-typo in models

--- projects/models.py
+++ projects/models.py
@@ -14,4 +14,4 @@ class Project(models.Model):
     image = models.ImageField(upload_to='img', blank=True, null=True)
 
     def __unicode__(self):
-        return self.titlecode
+        return self.title
