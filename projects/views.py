6bf331ce914b5b30a8b6a16976c8ae5eeed39d0d
Dave O'Dea <hi@daveodea.com>
2018-10-16 11:31:32 +0100

    Add get_projects view

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Projects
+from .forms import ProjectForm
 
-# Create your views here.
+def get_projects(request):
+    """
+    Create view with all existing Projects, which exist before now,
+    render them to the projects.html template
+    """
+    # Filter projects by published_date
+    projects = Projects.objects.filter(published_date__lte=timezone.now()
+                                        ).order_by('-published_date')
+    return render(request, "projects.html", {"projects": projects})
+
+
+
+def project_details(request, pk):
+    """
+    Creat view returning a Project object referenced by its PrimaryKey/ID,
+    render the Project details to projectdetails.html.
+    Return a 404 if Project is not found.
+    """
+
+def create_or_edit_project(request, pk=None):
+    """
+    Create view that can either create or edit a Project, 
+    edits if exists or creates if not. 
+    """
