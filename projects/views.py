bd4fbae51d437f8f31ab362edf64155eb0473db0
Dave O'Dea <hi@daveodea.com>
2019-01-16 14:29:59 +0000

    Add search template, alter search view

--- projects/views.py
+++ projects/views.py
@@ -81,7 +81,7 @@ def search_project(request):
     query = request.GET.get('search')
     results = Projects.objects.filter(Q(title__icontains=query) | Q(content__icontains=query))
 
-    return render(request, "index.html", {"projects": results})
+    return render(request, "search.html", {"projects": results})
 
 
 # Sharable URLs
