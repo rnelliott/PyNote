b27bfc4a0c70045db8a81f318c9eed9def31aa76
Dave O'Dea <hi@daveodea.com>
2019-01-17 16:22:32 +0000

    Add products model and initial test

--- products/models.py
+++ products/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200, default='')
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='img', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Product
 
 # Create your tests here.
+class ProductTests(TestCase):
+    """
+    Define tests that will run again the Product models
+    """
+
+    def test_str(self):
+        test_name = Product(name=" A Product")
+        self.assertEqual(str(test_name), "A Product")
