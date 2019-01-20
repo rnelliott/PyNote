6f90ed66f2fe67c82dc14b07d12966db4cfaecdc
Dave O'Dea <hi@daveodea.com>
2019-01-20 19:28:23 +0000

    Add context for sidenav projects, categories

--- products/views.py
+++ products/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from .models import Product
+from projects.models import Projects, Category
 
 
 # Create your views here.
 def all_products(request):
+    categories = Category.objects.filter(user__exact=request.user)
+    projects = Projects.objects.filter(user__exact=request.user)
     products = Product.objects.all()
-    return render(request, "products.html", {"products": products})
+    return render(request, "products.html", {"products": products,
+                                             "projects": projects,
+                                             "categories": categories})
