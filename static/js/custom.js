ba303885639760f8ef0017d948ce91f5815fb2e4
davedodea <hi@daveodea.com>
2019-02-18 17:27:29 +0000

    Remove console.logs

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,6 +1,5 @@
 // Back to projects -> 'back to blog' button
 $('.iD').click(function () {
-    console.log('clicked hai now!', $(this).attr('data-href'));
     var dataHref = $(this).attr('data-href')
     $("#ajax-content").load(dataHref)
     //$('#third').trigger('click');
