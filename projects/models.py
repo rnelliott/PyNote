09f1eb6dae17ffcf660f18e8894f03b7a7127770
davedodea <hi@daveodea.com>
2019-02-18 17:30:25 +0000

    Remove unnecessary imports and comments

--- projects/models.py
+++ projects/models.py
@@ -1,10 +1,9 @@
+from colorfield.fields import ColorField
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 from tinymce import HTMLField
-from colorfield.fields import ColorField
 
-import uuid
 
 class Category(models.Model):
     color = ColorField(default='#FF0000')
@@ -18,20 +17,15 @@ class Projects(models.Model):
     """
     Model for project/post by users
     """
-    # Create unique-sharable ID
-    # uuid = uuid.uuid4()
-    # uuid = models.CharField(max_length=64, default=uuid, unique=True)
     color = ColorField(default='#FF0000')
     category = models.ForeignKey('Category')
     is_sharable = models.BooleanField(default=False)
     user = models.ForeignKey(User, null=True, blank=True)
     title = models.CharField(max_length=200, verbose_name='Name')
-    # content = models.TextField()
     content = HTMLField('Content')
     created_date = models.DateTimeField(auto_now_add=True)
     published_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
     views = models.IntegerField(default=0)
-    # tag = models.CharField(max_length=30, blank=True, null=True)
 
     def __str__(self):
         return self.title
