3239109b529ec5d43abcf918f800234b7db809ff
davedodea <hi@daveodea.com>
2019-02-18 20:49:10 +0000

    Redirect logged in users away from login/register forms

--- accounts/views.py
+++ accounts/views.py
@@ -4,10 +4,11 @@ from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.shortcuts import HttpResponseRedirect, redirect, render
 from django.template.context_processors import csrf
-
 from home.views import home
+from projects.models import Category, Projects
+from projects.views import index
+
 from .forms import UserLoginForm, UserRegistrationForm
-from projects.models import Projects, Category
 
 
 def logout(request):
@@ -20,31 +21,34 @@ def logout(request):
 
 
 def login(request):
-    """A view that manages the login form"""
-    if request.method == 'POST':
-        user_form = UserLoginForm(request.POST)
-        if user_form.is_valid():
-            user = auth.authenticate(request.POST['username_or_email'],
-                                     password=request.POST['password'])
-
-            if user:
-                auth.login(request, user)
-                messages.error(request, "You have successfully logged in")
-                sweetify.success(request, 'You logged in!',
-                                 timer=1500, toast=True)
-                if request.GET and request.GET['next'] != '':
-                    next = request.GET['next']
-                    return HttpResponseRedirect(next)
-                else:
-                    return redirect(reverse('index'))
-            else:
-                user_form.add_error(
-                    None, "Your username or password are incorrect")
+    if request.user.is_authenticated():
+        return redirect(index)
     else:
-        user_form = UserLoginForm()
+        """A view that manages the login form"""
+        if request.method == 'POST':
+            user_form = UserLoginForm(request.POST)
+            if user_form.is_valid():
+                user = auth.authenticate(request.POST['username_or_email'],
+                                        password=request.POST['password'])
 
-    args = {'user_form': user_form, 'next': request.GET.get('next', '')}
-    return render(request, 'login.html', args)
+                if user:
+                    auth.login(request, user)
+                    messages.error(request, "You have successfully logged in")
+                    sweetify.success(request, 'You logged in!',
+                                    timer=1500, toast=True)
+                    if request.GET and request.GET['next'] != '':
+                        next = request.GET['next']
+                        return HttpResponseRedirect(next)
+                    else:
+                        return redirect(reverse('index'))
+                else:
+                    user_form.add_error(
+                        None, "Your username or password are incorrect")
+        else:
+            user_form = UserLoginForm()
+
+        args = {'user_form': user_form, 'next': request.GET.get('next', '')}
+        return render(request, 'login.html', args)
 
 
 @login_required
@@ -54,26 +58,29 @@ def profile(request):
 
 
 def register(request):
-    """A view that manages the registration form"""
-    if request.method == 'POST':
-        user_form = UserRegistrationForm(request.POST)
-        if user_form.is_valid():
-            user_form.save()
-
-            user = auth.authenticate(request.POST.get('email'),
-                                     password=request.POST.get('password1'))
-
-            if user:
-                auth.login(request, user)
-                messages.success(request, "You have successfully registered")
-                return redirect(reverse('index'))
-
-            else:
-                messages.error(request, "unable to log you in at this time!")
-                sweetify.error(
-                    request, 'Sorry, you couldn\'t be logged in at this time', timer=2000, toast=True)
+    if request.user.is_authenticated():
+        return redirect(index)
     else:
-        user_form = UserRegistrationForm()
+        """A view that manages the registration form"""
+        if request.method == 'POST':
+            user_form = UserRegistrationForm(request.POST)
+            if user_form.is_valid():
+                user_form.save()
 
-    args = {'user_form': user_form}
-    return render(request, 'register.html', args)
+                user = auth.authenticate(request.POST.get('email'),
+                                        password=request.POST.get('password1'))
+
+                if user:
+                    auth.login(request, user)
+                    messages.success(request, "You have successfully registered")
+                    return redirect(reverse('index'))
+
+                else:
+                    messages.error(request, "unable to log you in at this time!")
+                    sweetify.error(
+                        request, 'Sorry, you couldn\'t be logged in at this time', timer=2000, toast=True)
+        else:
+            user_form = UserRegistrationForm()
+
+        args = {'user_form': user_form}
+        return render(request, 'register.html', args)
